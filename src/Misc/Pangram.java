c9fc36c79fb71e07951452758d408034d90b2424
ReenuR <82420557+ReenuR@users.noreply.github.com>
2024-11-07 10:29:41 -0800

    commit

--- src/Misc/Pangram.java
+++ src/Misc/Pangram.java
@@ -1,13 +1,14 @@
 package Misc;
 
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Pangram {
     public static void main(String[] args) {
 
         System.out.println(checkIfPangram("TheQuickBrownFoxJumpsOverTheLazyDog23"));
     }
-    public static boolean checkIfPangram(String sentence){
+    /*public static boolean checkIfPangram(String sentence){
         if(sentence.length()<26)
             return false;
          char[] charArray =  sentence.toLowerCase().toCharArray();
@@ -20,5 +21,24 @@ public class Pangram {
          }
         return map.size()>=26;
 
+    }*/
+
+    public static boolean checkIfPangram(String sentence){
+        // Create a set to store unique characters
+        Set<Character> seen = new HashSet<>();
+
+        // Iterate over each character using a normal for loop
+        for (int i = 0; i < sentence.length(); i++) {
+            // Convert the current character to lowercase
+            char currChar = Character.toLowerCase(sentence.charAt(i));
+
+            if (Character.isLetter(currChar)) {
+                // Add the character to the set
+                seen.add(currChar);
+            }
+        }
+
+        // Return true if set size is 26 (total number of alphabets)
+        return seen.size() == 26;
     }
 }
